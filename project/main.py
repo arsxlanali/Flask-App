9bbb80e062af38b6e5ad92fbc2df93d013d87ff7
arsxlanali <arsxlanali@gmail.com>
2022-12-31 17:48:48 +0500

    Added the shop and details pages

--- project/main.py
+++ project/main.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
+import sqlite3
+from .models import shop
 from . import db
-
 main = Blueprint('main', __name__)
 
 
@@ -10,12 +11,42 @@ def index():
     return render_template('index.html')
 
 
+
+
 @main.route('/home')
-@login_required
 def home():
     return render_template('home.html', name=current_user.name)
 
+@main.route('/aboutus')
+def aboutus():
+    return render_template('aboutus.html', name=current_user.name)
 
+
+
+breed = ['Red', 'Blue', 'Black', 'Breed']
+size = ['Red', 'Blue', 'Black', 'Size']
+@main.route('/shop')
+def shop():
+    con = sqlite3.connect('instance\db.sqlite')
+    cur = con.cursor()    
+    cur.execute("SELECT * FROM shop;")
+    items = cur.fetchall()
+    return render_template('shop.html', items=items, len=len(items), breed=breed, size=size)
+
+@main.route('/shop/<id>')
+def shopDetials(id):
+    id = 3
+    con = sqlite3.connect('instance\db.sqlite')
+    cur = con.cursor()    
+    cur.execute("SELECT * FROM shop where id = ?", (id,))
+    row = cur.fetchone()
+    postUrl = row[1]
+    title = row[2]
+    details = row[3]
+    review = row[4]
+    imgUrl = row[5]
+    return render_template('shopdetails.html', postUrl=postUrl,title=title,
+                           details=details, review=review,imgUrl=imgUrl)
 @main.route('/profile')
 @login_required
 def profile():
--- project/models.py
+++ project/models.py
@@ -8,3 +8,13 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
+
+class shop(db.Model):
+    # primary keys are required by SQLAlchemy
+    id = db.Column(db.Integer, primary_key=True)
+    postUrl = db.Column(db.String(100))
+    title = db.Column(db.String(100))
+    details = db.Column(db.String(1000))
+    review = db.Column(db.String(1000))
+    imageUrl = db.Column(db.String(1000))
+
